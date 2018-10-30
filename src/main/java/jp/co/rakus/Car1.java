16daf203d615fe505baa09ccee301480d4f13ab5
kitano <maiko.kitano@rakus.co.jp>
2018-10-30 11:39:20 +0900

    インデントの調整

--- src/main/java/jp/co/rakus/Car1.java
+++ src/main/java/jp/co/rakus/Car1.java
@@ -1,21 +1,24 @@
 package jp.co.rakus;
 
 public class Car1 {
-	
+
 	private Integer speed;
 	private String bodyColor;
+
 	public Integer getSpeed() {
 		return speed;
 	}
+
 	public void setSpeed(Integer speed) {
 		this.speed = speed;
 	}
+
 	public String getBodyColor() {
 		return bodyColor;
 	}
+
 	public void setBodyColor(String bodyColor) {
 		this.bodyColor = bodyColor;
 	}
-	
 
 }
