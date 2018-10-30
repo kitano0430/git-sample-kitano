1a20f00b494716bcc614abb228e289f469a51232
kitano <maiko.kitano@rakus.co.jp>
2018-10-30 10:58:56 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/jp/co/rakus/GitSampleKitanoApplication.java
@@ -0,0 +1,12 @@
+package jp.co.rakus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKitanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKitanoApplication.class, args);
+	}
+}
