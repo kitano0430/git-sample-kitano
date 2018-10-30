987c2cdbae7035ce8856d88dc8d98de838dad6dd
kitano <maiko.kitano@rakus.co.jp>
2018-10-30 11:27:11 +0900

    新規Carクラス作成及びスタートプログラムにコメントを残すよう修正

---
+++ src/main/java/jp/co/rakus/Car.java
@@ -0,0 +1,22 @@
+package jp.co.rakus;
+
+/**
+ * 
+ * 
+ 車を表すドメイン
+ * @author rksuser
+ *
+ */
+public class Car {
+	
+	private Integer speed;
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+
+}
--- src/main/java/jp/co/rakus/GitSampleKitanoApplication.java
+++ src/main/java/jp/co/rakus/GitSampleKitanoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitSampleKitanoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("デプロイとs－バー起動を行います");
 		SpringApplication.run(GitSampleKitanoApplication.class, args);
 	}
 }
